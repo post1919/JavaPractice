c5ec89b305deb6467b8f104a4a413ad021e9e1a8
DongAh <post1919@naver.com>
2021-05-31 02:00:40 +0900

    hello world4

--- src/main/java/com/dongah/stream/StreamEx1.java
+++ src/main/java/com/dongah/stream/StreamEx1.java
@@ -3,5 +3,6 @@ package com.dongah.stream;
 public class StreamEx1 {
     public static void main(String[] args) {
         System.out.println("hello World!");
+        System.out.println("Hello World4");
     }
 }
