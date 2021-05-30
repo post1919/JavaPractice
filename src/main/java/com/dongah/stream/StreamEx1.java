79647e855d4e97a7c7ad4f690be8e247473c36fb
DongAh <post1919@naver.com>
2021-05-31 01:58:45 +0900

    20210531_temp에 추가

--- src/main/java/com/dongah/stream/StreamEx1.java
+++ src/main/java/com/dongah/stream/StreamEx1.java
@@ -3,5 +3,7 @@ package com.dongah.stream;
 public class StreamEx1 {
     public static void main(String[] args) {
         System.out.println("hello World!");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
     }
 }
