f5f0a810522f02936a5b7321fc3d8ddb85a4f80a
dongah.shin <post1919@naver.com>
2021-07-12 12:45:48 +0900

    빌더패턴 기존 에제 삭제 빌더패턴 예제추가

--- src/main/java/com/dongah/pattern/BuilderPatternEx.java
+++ src/main/java/com/dongah/pattern/BuilderPatternEx.java
@@ -2,16 +2,10 @@ package com.dongah.pattern;
 
 public class BuilderPatternEx {
     public static void main(String[] args) {
-        UserInfo2 userInfo = UserInfo2.builder()
-                .userId("1")
-                .userName("2")
-                .password("3")
-                .nickname("4")
-                .build();
-
-        System.out.println(userInfo.getUserId());
-        System.out.println(userInfo.getUserName());
-        System.out.println(userInfo.getPassword());
-        System.out.println(userInfo.getNickname());
+        BuilderPatternModel model = BuilderPatternModel.builder().userId("1").userName("2").password("3").email("4").build();
+        System.out.println(model.getUserId());
+        System.out.println(model.getUserName());
+        System.out.println(model.getPassword());
+        System.out.println(model.getEmail());
     }
 }
