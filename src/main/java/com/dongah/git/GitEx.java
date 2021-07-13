14fc17953fdff3375d68028e2185491bd934c383
dongah.shin <post1919@naver.com>
2021-07-13 16:40:41 +0900

    git0002 메서드 추가

--- src/main/java/com/dongah/git/GitEx.java
+++ src/main/java/com/dongah/git/GitEx.java
@@ -2,13 +2,9 @@ package com.dongah.git;
 
 public class GitEx {
     public static void main(String[] args) {
-
-
-        System.out.println("GIT-0002 브랜치에서 작성");
+        System.out.println("GIT-0001 브랜치에서 수정");
         System.out.println("develop브랜치에 Merge");
     }
 
-
-
-    public void git0002(){}
+    public void git0001(){}
 }
