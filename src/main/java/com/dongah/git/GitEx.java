f0d6e086bfe18c544ba48c03aefe44e35dda6f46
dongah.shin <post1919@naver.com>
2021-07-13 15:57:27 +0900

    GIT-0001

--- src/main/java/com/dongah/git/GitEx.java
+++ src/main/java/com/dongah/git/GitEx.java
@@ -2,7 +2,7 @@ package com.dongah.git;
 
 public class GitEx {
     public static void main(String[] args) {
-        System.out.println("GIT-0002 브랜치에서 작성");
+        System.out.println("GIT-0001 브랜치에서 작성");
         System.out.println("develop브랜치에 Merge");
     }
 }
