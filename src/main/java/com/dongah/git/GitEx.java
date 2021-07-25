4d850ccf46f4e447cc411c074c5c55c6c655bc3f
dongah.shin <post1919@naver.com>
2021-07-25 17:17:53 +0900

    release-v1.07.26

--- src/main/java/com/dongah/git/GitEx.java
+++ src/main/java/com/dongah/git/GitEx.java
@@ -3,6 +3,7 @@ package com.dongah.git;
 public class GitEx {
     public static void main(String[] args) {
         System.out.println("release-v1.07.25");
+        System.out.println("release-v1.07.26");
     }
 
     public void git0001(){}
