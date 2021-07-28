2fb3890da3c3016815b8a674970db144b955a67f
dongah.shin <post1919@naver.com>
2021-07-29 00:40:59 +0900

    feature/ONRI-00004

--- src/main/java/com/dongah/git/Onri00002.java
+++ src/main/java/com/dongah/git/Onri00002.java
@@ -3,5 +3,6 @@ package com.dongah.git;
 public class Onri00002 {
     public void onri00002(){
         System.out.println("ONRI-00002");
+        System.out.println("ONRI-00003 브랜치에 추가함");
     }
 }
