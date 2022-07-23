b59af129077ce2dc886982034819de810c36c075
신동아 <dashin@castingn.com>
2022-07-24 02:59:21 +0900

    Revert "기능1"
    This reverts commit 3b00d957

--- src/main/java/com/dongah/thread/ThreadCall.java
+++ src/main/java/com/dongah/thread/ThreadCall.java
@@ -6,7 +6,6 @@ public class ThreadCall implements Runnable {
         try {
             Thread.sleep(100); // 0.1초간 스레드를 멈춤.
             //기능1
-            //Reset 후 기능2
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
