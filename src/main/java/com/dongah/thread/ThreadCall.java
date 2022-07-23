b779f0da4787dcee1df0877b27cbffe59d4a0518
신동아 <dashin@castingn.com>
2022-07-24 00:39:04 +0900

    //feature/A 기능4 -> 4

--- src/main/java/com/dongah/thread/ThreadCall.java
+++ src/main/java/com/dongah/thread/ThreadCall.java
@@ -5,7 +5,10 @@ public class ThreadCall implements Runnable {
     public void run() {
         try {
             Thread.sleep(100); // 0.1초간 스레드를 멈춤.
-            //Master 기능1
+            //feature/A 기능1
+            //feature/A 기능2
+            //feature/A 기능3
+            //feature/A 기능4
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
