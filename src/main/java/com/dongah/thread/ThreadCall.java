f86455017e7b5848234d98bed1a099521c4c88d9
신동아 <dashin@castingn.com>
2022-07-23 13:54:26 +0900

    //feature/A 기능3 체리픽

--- src/main/java/com/dongah/thread/ThreadCall.java
+++ src/main/java/com/dongah/thread/ThreadCall.java
@@ -5,6 +5,9 @@ public class ThreadCall implements Runnable {
     public void run() {
         try {
             Thread.sleep(100); // 0.1초간 스레드를 멈춤.
+            //feature/A 기능1
+            //feature/A 기능2
+            //feature/A 기능3
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
